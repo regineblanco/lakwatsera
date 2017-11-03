784ccc3dfd4be6fdb7935500845181a44024b303
Regine Blanco <regineblanco@regineblanco.local>
2017-11-03 14:26:28 +0800

    Added categories and interests tables

--- resources/views/questions/second.blade.php
+++ resources/views/questions/second.blade.php
@@ -3,53 +3,142 @@
 @section('content')
 
 <div class="second_question">
-	<div class="container">
+	<form> 
+		<p>Let's get to know you more. Tell us your according to them.</p>
 		<div class="row">
-			<div class="col-xs-12 col-md-3 interests">
-				<div class="interests_category">
-					<ul class="d-flex flex-column">
-						<li><a onclick="arts()">Arts &amp; Culture</a></li>
-						<li><a href="">Food</a></li>
-						<li><a href="">Shopping</a></li>
-						<li><a href="">Music</a></li>
-						<li><a href="">Recreation</a></li>
-						<li><a href="">Nightlife</a></li>
-						<li><button class="btn btn-light">I'm Done!</button></li>
-					</ul>
-				</div><!-- close interests_category -->
-			</div><!-- close interests -->
-			<div class="col-xs-12 col-md-9 interests_list">
-				<p>Let's get to know you more. Tell us your interests, so we can tailor our suggestions according to them.</p>
+			<div class="col-xs-12 col-sm-6 col-lg-4 interests" id="arts">
+				<h4>Arts &amp; Culture</h4>	
+				<div class="col-xs-12 col-sm-6 checkbox">
+					<input type="checkbox" name="" />Museum
+				</div>
+				<div class="col-xs-12 col-sm-6 checkbox">
+					<input type="checkbox" name="" />Historical sites and monuments
+				</div>
+				<div class="col-xs-12 col-sm-6 checkbox">
+					<input type="checkbox" name="" />Art galleries and exhibits
+				</div>
+				<div class="col-xs-12 col-sm-6 checkbox">
+					<input type="checkbox" name="" />Churches
+				</div>
+				<div class="col-xs-12 col-sm-6 checkbox">
+					<input type="checkbox" name="" />All of the above!
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-lg-4 interests" id="food">
+				<h4>Food</h4>
 				<div class="row">
-					<div id="arts">
-						<div class="col-xs-12 col-md-6 interests_item">
-							<img src="{{ asset('img/manila.jpg') }}">
-						</div>
-						<div class="col-xs-12 col-md-6 interests_item">
-							<img src="{{ asset('img/manila.jpg') }}">
-						</div>
-						<div class="col-xs-12 col-md-6 interests_item">
-							<img src="{{ asset('img/manila.jpg') }}">
-						</div>
-						<div class="col-xs-12 col-md-6 interests_item">
-							<img src="{{ asset('img/manila.jpg') }}">
-						</div>
-						<div class="col-xs-12 col-md-6 interests_item">
-							<img src="{{ asset('img/manila.jpg') }}">
-						</div>
-						<div class="col-xs-12 col-md-6 interests_item">
-							<img src="{{ asset('img/manila.jpg') }}">
-						</div>
-						<div class="col-xs-12 col-md-6 interests_item">
-							<img src="{{ asset('img/manila.jpg') }}">
-						</div>
-						<div class="col-xs-12 col-md-6 interests_item">
-							<img src="{{ asset('img/manila.jpg') }}">
-						</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Pinoy cuisine
 					</div>
-				</div><!-- close row interests_item -->
-			</div><!-- close interests_list -->
-		</div><!-- close row second_question -->
-	</div><!-- close container second_question -->
-</div><!-- close second_quetion -->
-
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Asian cuisine
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Western cuisine
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Food parks and markets
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Wet and seafood markets
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Coffee and dessert shops
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />All of the above!
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-lg-4 interests" id="shopping">
+				<h4>Shopping</h4>
+				<div class="row">
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Art markets and eccentric finds
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Flee markets and cheap finds
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Themed markets (flowers, pets, fruits)
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />High-end malls
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />All of the above!
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-lg-4 interests" id="music">
+				<h4>Music</h4>
+				<div class="row">
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Live acoustic
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Live jazz
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Live rock/alternative
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Local indie music scene
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Underground music
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Stage musicals
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Karaoke
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />All of the above!
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-lg-4 interests" id="recreation">
+				<h4>Recreation</h4>
+				<div class="row">
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Water sports (swimming, wakeboarding)
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Escape rooms and games
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Amusement parks
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />All of the above!
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-lg-4 interests" id="nightlife">
+				<h4>Nightlife</h4>
+				<div class="row">
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Bars and speakeasies
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Comedy bars
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Nightclubs
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Underground clubs
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />Music festivals
+					</div>
+					<div class="col-xs-12 col-sm-6 checkbox">
+						<input type="checkbox" name="" />All of the above!
+					</div>
+				</div>
+			</div>
+		</div>
+	</form>
+</div>
